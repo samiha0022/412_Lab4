aa593f34460c1a698d6bc6645390d39d34bd3007
samiha0022 <mahjabinsamiha547@gmail.com>
2025-03-19 22:58:56 +0600

    Update 207 ass 5.c

--- 207 ass 5.c
+++ 207 ass 5.c
@@ -55,18 +55,13 @@ void nextGreaterElement(int arr[], int n) {
 
 int main() {
     int n;
-
     printf("Enter number of elements: ");
     scanf("%d", &n);
-
     int arr[n];
-
     printf("Enter elements:\n");
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-
     nextGreaterElement(arr, n);
-
     return 0;
 }
